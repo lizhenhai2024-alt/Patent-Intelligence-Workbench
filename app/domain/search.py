bd1e983b94cc15ec12d93a9e5a317ee36e6101d8
Zhenhai Li/Bruce <lizhenhai2024@gmail.com>
2026-09-20 17:46:09 +0800

    feat: add ANDed technology concept groups

--- app/domain/search.py
+++ app/domain/search.py
@@ -18,6 +18,7 @@ class SearchMode(StrEnum):
 class SearchExpression:
     applicants: tuple[str, ...] = ()
     text_terms: tuple[str, ...] = ()
+    text_groups: tuple[tuple[str, ...], ...] = ()
     jurisdictions: tuple[str, ...] = ()
     published_from: date | None = None
     published_to: date | None = None
